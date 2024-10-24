3df5718a79d80f1b082a966afa22d2c2463fc6b7
Arekhandia Harry <52859960+potterinc@users.noreply.github.com>
2024-10-24 12:47:24 +0100

    removed mysql2 dependency

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,33 +1,33 @@
-import mysql, { Pool } from 'mysql2/promise'
+// import mysql, { Pool } from 'mysql2/promise'
 import AppConfig from './app.config';
 
 
 /**@description MySQL Server connection configuration */
 class DatabaseConnectionPool {
-  db: Pool;
+  // db: Pool;
 
   constructor() {
 
-    this.db = mysql.createPool({
-      host: AppConfig.database.HOST,
-      user: AppConfig.database.USER,
-      password: AppConfig.database.PASSWORD,
-      database: 'ProofOfConcept',
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
+    // this.db = mysql.createPool({
+    //   host: AppConfig.database.HOST,
+    //   user: AppConfig.database.USER,
+    //   password: AppConfig.database.PASSWORD,
+    //   database: 'ProofOfConcept',
+    //   waitForConnections: true,
+    //   connectionLimit: 10,
+    //   queueLimit: 0
+    // });
 
     this.Init();
   }
 
   // Initializing connection pool
   private async Init() {
-    await this.db.getConnection()
-      .then(con => {
-        console.table([{ HOST: con.connection.config.host, DATASOURCE: con.connection.config.database }])
-      })
-      .catch(e => console.error(e))
+    // await this.db.getConnection()
+    //   .then(con => {
+    //     console.table([{ HOST: con.connection.config.host, DATASOURCE: con.connection.config.database }])
+    //   })
+    //   .catch(e => console.error(e))
   }
 }
 
